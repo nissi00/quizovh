edda77c00291bd16b1e258b2caa858eac128a398
NISSI CHRIST EMMANUEL ETIEN <108407685+nissi00@users.noreply.github.com>
2026-09-16 09:57:34 +0200

    Load statistics routes

--- server/index-with-survey.js
+++ server/index-with-survey.js
@@ -1,5 +1,6 @@
 import './survey-bootstrap.js';
 import './lot-improvements-bootstrap.js';
 import './quality-bootstrap.js';
+import './statistics-bootstrap.js';
 import './shared-staff-access-bootstrap.js';
 await import('./index.js');
